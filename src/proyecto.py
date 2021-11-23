ed00e8c0235a5d1d9ca12afbc641af99c2b24c90
Andres Yañez <aandresfelipeyanezz@gmail.com>
2021-11-23 13:59:02 -0500

    FinalVersion

--- src/proyecto.py
+++ src/proyecto.py
@@ -6,6 +6,7 @@ import plotly.express as px
 import pandas as pd
 from Connection import Connection
 import proyectoSQL as sql
+import requests
 
 external_stylesheets = ["https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"]
 
